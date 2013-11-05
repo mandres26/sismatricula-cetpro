4ba743a34b5e7875556a35572d3dc4f1d56ffd2c
jdiego.947@gmail.com <jdiego.947@gmail.com@2609c05c-3ae7-d2db-8edb-9e5be9db6e07>
2013-11-05 07:28:05 +0000

    CUS - Registrar Resolución Directoral

---
+++ trunk/SisMatriculaCETPRO/src/java/com/control/regResolucionDir_Control.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.control;
+
+/**
+ *
+ * @author Juan Diego
+ */
+public class regResolucionDir_Control {
+    private String numResolucion;
+
+    /**
+     * @return the numeroResolucion
+     */
+    public String getNumeroResolucion() {
+        return numResolucion;
+    }
+
+    /**
+     * @param numeroResolucion the numeroResolucion to set
+     */
+    public void setNumeroResolucion(String numeroResolucion) {
+        this.numResolucion = numeroResolucion;
+    }
+    
+    
+}
+
