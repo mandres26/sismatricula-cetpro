296732cc5077f234f47da5c86b73c5d005cc3d65
jamescentenol@gmail.com <jamescentenol@gmail.com@2609c05c-3ae7-d2db-8edb-9e5be9db6e07>
2013-11-05 20:29:15 +0000

    Mantener Especialidad en Resolucion Directoral
    by James

---
+++ trunk/SisMatriculaCETPRO/src/java/com/control/manEspecialidad_Control.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.control;
+
+/**
+ *
+ * @author james
+ */
+public class manEspecialidad_Control {
+    private String codEspecialidad;
+
+    /**
+     * @return the codigoEspecialidad
+     */
+    public String getCodigoEspecialidad() {
+        return codEspecialidad;
+    }
+
+    /**
+     * @param codigoEspecilidad the codigoEspecialidad to set
+     */
+    public void setCodigoEspecialidad(String codigoEspecialidad) {
+        this.codEspecialidad = codigoEspecialidad;
+    }
+    
+}
